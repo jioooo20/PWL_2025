de907f1e3bd26e5b2d018af485fbe207e6433abf
Yeon <giojio825@gmail.com>
2025-03-03 10:32:42 +0700

    Update UserController to create a new user and check for changes in user attributes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,16 +11,28 @@ class UserController extends Controller
     public function index()
     {
 
-        $User = UserModel::firstOrNew(
+        $User = UserModel::create(
             [
-                'username' => 'manager33',
-                'nama' => 'Manager tiga tiga',
+                'username' => 'manager11',
+                'nama' => 'Manager 11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
 
+        $User->username = 'manager12';
+
         $User->save();
+
+        $User->wasChanged(); // true
+        $User->wasChanged('username'); // true
+        $User->wasChanged(['username', 'level_id']); // true
+        $User->wasChanged(['nama']); // false
+        dd($User->wasChanged(['nama', 'username'])); //true
+
+
+
+
 
         return view('user', ['data' => $User]);
 
